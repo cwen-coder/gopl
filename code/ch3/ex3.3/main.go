36d7b6831c6d33feabf3f2ca1d1dccbd3a6fb633
cwen-coder <172569856@qq.com>
2016-01-30 23:32:22 +0800

    ch3

---
+++ code/ch3/ex3.3/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net/http"
+)
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+const (
+	red  = "#FF0000"
+	blue = "#000FF"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		surface(w)
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func surface(out io.Writer) {
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, _ := corner(i+1, j)
+			bx, by, _ := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, isPeek := corner(i+1, j+1)
+			color := blue
+			if isPeek {
+				color = red
+			}
+			fmt.Fprintf(out, "<polygon style='fill: %s' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				color, ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Fprintln(out, "</svg>")
+}
+func corner(i, j int) (float64, float64, bool) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	var isPeek bool
+	if z >= 0 {
+		isPeek = true
+	}
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, isPeek
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+
+	if r != 0 {
+		return math.Sin(r) / r
+	} else {
+		return 0
+	}
+}
