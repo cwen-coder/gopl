f05014d0113fce1a829c560b2d8fd24b69b85f56
cwen-coder <172569856@qq.com>
2016-01-30 14:33:44 +0800

    ch3

---
+++ code/ch2/ex2.1/conv.go
@@ -0,0 +1,20 @@
+package tempconv
+
+import (
+	"fmt"
+)
+
+type KelvinTemperature float64
+type CelsiusTemperature float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+)
+
+func KtoC(k KelvinTemperature) CelsiusTemperature {
+	return CelsiusTemperature(k - AbsoluteZeroC)
+}
+
+func CtoC(c CelsiusTemperature) KelvinTemperature {
+	return KelvinTemperature(c + AbsoluteZeroC)
+}
---
+++ code/ch3/test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var z float64
+	fmt.Println(z, -z, 1/z, -1/z, z/z)
+}
